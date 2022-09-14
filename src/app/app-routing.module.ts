22551d31626ff88abe10900bc2661bf62df7251d
Elihú SANDOVAL <ESANDOVAL@suppliers.tenaris.com>
2022-09-14 13:56:24 -0500

    Add Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './pages/home/home.component';
+import { ApiComponent } from './pages/api/api.component';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent},
+  { path: 'api', component: ApiComponent}
 ];
 
 @NgModule({
